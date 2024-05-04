08860288000ba96b1ee12a1d2eda47ee16edd9ae
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-04 12:51:28 +0530

    commit

---
+++ MainFile/objects.js
@@ -0,0 +1,16 @@
+let i=1;
+
+const object = {
+  [`${i++}`]: {
+    link: 'https://www.youtube.com/',
+    p:'BlockChain Project with NextJs',
+    sp:'Chat DAPP'
+  },
+  [`${i++}`]: {
+    link: '#',
+    p:'Project Title',
+    sp:'Project Name'
+  },
+}
+
+export default object;
