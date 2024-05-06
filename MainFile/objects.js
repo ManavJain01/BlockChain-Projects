4f477c25238390af4d109c6edd94ef316a3d2167
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-06 22:42:51 +0530

    commit

--- MainFile/objects.js
+++ MainFile/objects.js
@@ -3,11 +3,13 @@ let i=1;
 const object = {
   [`${i++}`]: {
     link: 'https://www.youtube.com/',
+    image: './Images/Project Images/Chat_DAPP.png',
     p:'BlockChain Project with NextJs',
     sp:'Chat DAPP'
   },
   [`${i++}`]: {
     link: '#',
+    image: null,
     p:'Project Title',
     sp:'Project Name'
   },
